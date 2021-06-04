63be955c5acd17466ca2ba2c3b8df67dfacc5e7b
chengke <chengke@uefee1b00be3a55.ant.amazon.com>
2021-06-04 15:43:00 -0700

    test1

--- robomaker-sample-app-ci/src/aws-robomaker-sample-application-ci.ts
+++ robomaker-sample-app-ci/src/aws-robomaker-sample-application-ci.ts
@@ -79,7 +79,7 @@ async function getSampleAppVersion() : Promise<string> {
   return Promise.resolve(version);
 }
 
-// If .rosinstall exists, run 'vcs import' and return a list of names of the packages that were added in both workspaces.
+// // If .rosinstall exists, run 'vcs import' and return a list of names of the packages that were added in both workspaces.
 async function fetchRosinstallDependencies(): Promise<string[]> {
   let colconListAfter = {stdout: '', stderr: ''};
   let packages: string[] = [];
@@ -105,75 +105,92 @@ async function fetchRosinstallDependencies(): Promise<string[]> {
   }
   return Promise.resolve(packages);
 }
+ async function setup() {
+   try{
 
-async function setup() {
-  try {
-    await exec.exec("sudo", ["apt-key", "adv", "--fetch-keys", "http://packages.osrfoundation.org/gazebo.key"]);
+      //check if timezone is setup.exec
+      //#timezone is required for ROS Installation.
+    // if [ ! -f "/etc/timezone" ];
+    // then
+    //   export TZ="US/Pacific"
+    //   ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
-    let aptPackages = [
-      "zip",
-      "cmake",
-      "lcov",
-      "libgtest-dev",
-      "python3-colcon-common-extensions",
-      "python3-apt",
-      "python3-pip",
-      (ROS_DISTRO == "foxy") ? "python3-rosinstall" : "python-rosinstall",
-    ];
-
-    if(ROS_DISTRO!="foxy"){
-      //focal (foxy) does not ship with python2 and does not require python-pip
-      //using the ros_distro instead of ubuntu_distro saves users from specifying another 
-      //essentially redundant parameter.
-      aptPackages = aptPackages.concat(["python-pip"])
-    }
-
-    const python3Packages = [
-      "setuptools",
-      "colcon-bundle",
-      "colcon-ros-bundle"
-    ];
-
-    await exec.exec("sudo", ["apt-get", "update"]);
-    await exec.exec("sudo", ["apt-get", "install", "-y"].concat(aptPackages));
-    await exec.exec("sudo", ["pip3", "install", "-U"].concat(python3Packages));
-
-    await exec.exec("rosdep", ["update"]);
-
-    await loadROSEnvVariables();
-
-    SAMPLE_APP_VERSION = await getSampleAppVersion();
-    console.log(`Sample App version found to be: ${SAMPLE_APP_VERSION}`);
-
-    // Update PACKAGES_TO_SKIP_TESTS with the new packages added by 'rosws update'.
-    let packages = await fetchRosinstallDependencies();
-    PACKAGES = packages.join(" ");
-  } catch (error) {
-    core.setFailed(error.message);
-  }
-}
-
-async function setup_gazebo_source() {
-  try {
-    const gazebo_apt_file = "/etc/apt/sources.list.d/gazebo-stable.list";
-    await exec.exec("sudo", ["rm", "-f", gazebo_apt_file]);
-    await exec.exec("bash", ["-c", `echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable \`lsb_release -cs\` main" | sudo tee ${gazebo_apt_file}`]);
-    await exec.exec("sudo", ["apt-get", "update"]);
-
-    if (ROS_DISTRO == "kinetic") {
-      const gazebo9_rosdep_file = "/etc/ros/rosdep/sources.list.d/00-gazebo9.list";
-      await exec.exec("sudo", ["rm", "-f", gazebo9_rosdep_file]);
-      await exec.exec("bash", ["-c", `echo "yaml https://github.com/osrf/osrf-rosdep/raw/master/gazebo9/gazebo.yaml" | sudo tee -a ${gazebo9_rosdep_file}`]);
-      await exec.exec("bash", ["-c", `echo "yaml https://github.com/osrf/osrf-rosdep/raw/master/gazebo9/releases/indigo.yaml indigo" | sudo tee -a ${gazebo9_rosdep_file}`]);
-      await exec.exec("bash", ["-c", `echo "yaml https://github.com/osrf/osrf-rosdep/raw/master/gazebo9/releases/jade.yaml jade" | sudo tee -a ${gazebo9_rosdep_file}`]);
-      await exec.exec("bash", ["-c", `echo "yaml https://github.com/osrf/osrf-rosdep/raw/master/gazebo9/releases/kinetic.yaml kinetic" | sudo tee -a ${gazebo9_rosdep_file}`]);
-      await exec.exec("bash", ["-c", `echo "yaml https://github.com/osrf/osrf-rosdep/raw/master/gazebo9/releases/lunar.yaml lunar" | sudo tee -a ${gazebo9_rosdep_file}`]);
-      await exec.exec("rosdep", ["update"]);
-    }
+      await exec.exec("sudo", ["source", "../scripts/setup.sh"]);
+      let packages = await fetchRosinstallDependencies();
+      PACKAGES = packages.join(" ");
    } catch (error) {
     core.setFailed(error.message);
-  }
-}
+   }
+
+ }
+// async function setup() {
+//   try {
+//     await exec.exec("sudo", ["apt-key", "adv", "--fetch-keys", "http://packages.osrfoundation.org/gazebo.key"]);
+
+//     let aptPackages = [
+//       "zip",
+//       "cmake",
+//       "lcov",
+//       "libgtest-dev",
+//       "python3-colcon-common-extensions",
+//       "python3-apt",
+//       "python3-pip",
+//       (ROS_DISTRO == "foxy") ? "python3-rosinstall" : "python-rosinstall",
+//     ];
+
+//     if(ROS_DISTRO!="foxy"){
+//       //focal (foxy) does not ship with python2 and does not require python-pip
+//       //using the ros_distro instead of ubuntu_distro saves users from specifying another 
+//       //essentially redundant parameter.
+//       aptPackages = aptPackages.concat(["python-pip"])
+//     }
+
+//     const python3Packages = [
+//       "setuptools",
+//       "colcon-bundle",
+//       "colcon-ros-bundle"
+//     ];
+
+//     await exec.exec("sudo", ["apt-get", "update"]);
+//     await exec.exec("sudo", ["apt-get", "install", "-y"].concat(aptPackages));
+//     await exec.exec("sudo", ["pip3", "install", "-U"].concat(python3Packages));
+
+//     await exec.exec("rosdep", ["update"]);
+
+//     await loadROSEnvVariables();
+
+//     SAMPLE_APP_VERSION = await getSampleAppVersion();
+//     console.log(`Sample App version found to be: ${SAMPLE_APP_VERSION}`);
+
+//     // Update PACKAGES_TO_SKIP_TESTS with the new packages added by 'rosws update'.
+//     let packages = await fetchRosinstallDependencies();
+//     PACKAGES = packages.join(" ");
+//   } catch (error) {
+//     core.setFailed(error.message);
+//   }
+// }
+
+// async function setup_gazebo_source() {
+//   try {
+//     const gazebo_apt_file = "/etc/apt/sources.list.d/gazebo-stable.list";
+//     await exec.exec("sudo", ["rm", "-f", gazebo_apt_file]);
+//     await exec.exec("bash", ["-c", `echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable \`lsb_release -cs\` main" | sudo tee ${gazebo_apt_file}`]);
+//     await exec.exec("sudo", ["apt-get", "update"]);
+
+//     if (ROS_DISTRO == "kinetic") {
+//       const gazebo9_rosdep_file = "/etc/ros/rosdep/sources.list.d/00-gazebo9.list";
+//       await exec.exec("sudo", ["rm", "-f", gazebo9_rosdep_file]);
+//       await exec.exec("bash", ["-c", `echo "yaml https://github.com/osrf/osrf-rosdep/raw/master/gazebo9/gazebo.yaml" | sudo tee -a ${gazebo9_rosdep_file}`]);
+//       await exec.exec("bash", ["-c", `echo "yaml https://github.com/osrf/osrf-rosdep/raw/master/gazebo9/releases/indigo.yaml indigo" | sudo tee -a ${gazebo9_rosdep_file}`]);
+//       await exec.exec("bash", ["-c", `echo "yaml https://github.com/osrf/osrf-rosdep/raw/master/gazebo9/releases/jade.yaml jade" | sudo tee -a ${gazebo9_rosdep_file}`]);
+//       await exec.exec("bash", ["-c", `echo "yaml https://github.com/osrf/osrf-rosdep/raw/master/gazebo9/releases/kinetic.yaml kinetic" | sudo tee -a ${gazebo9_rosdep_file}`]);
+//       await exec.exec("bash", ["-c", `echo "yaml https://github.com/osrf/osrf-rosdep/raw/master/gazebo9/releases/lunar.yaml lunar" | sudo tee -a ${gazebo9_rosdep_file}`]);
+//       await exec.exec("rosdep", ["update"]);
+//     }
+//    } catch (error) {
+//     core.setFailed(error.message);
+//   }
+// }
 
 async function prepare_sources() {
   try {
@@ -240,22 +257,22 @@ async function run() {
   }
 
   await setup();
-  if (ROS_DISTRO == "kinetic" && (GAZEBO_VERSION == "" || GAZEBO_VERSION == "7")) {
-    GAZEBO_VERSION = "7";
-  } else if (ROS_DISTRO == "kinetic" && GAZEBO_VERSION == "9") {
-    await setup_gazebo_source();
-  } else if (ROS_DISTRO == "melodic" && (GAZEBO_VERSION == "" || GAZEBO_VERSION == "9")) {
-    GAZEBO_VERSION = "9";
-    await setup_gazebo_source();
-  } else if (ROS_DISTRO == "dashing" && (GAZEBO_VERSION == "" || GAZEBO_VERSION == "9")) {
-    GAZEBO_VERSION = "9";
-    await setup_gazebo_source();
-  } else if (ROS_DISTRO == "foxy" && (GAZEBO_VERSION == "" || GAZEBO_VERSION == "11")) {
-    GAZEBO_VERSION = "11";
-    await setup_gazebo_source();
-  } else {
-    core.setFailed(`Invalid ROS and Gazebo combination`);
-  }
+  // if (ROS_DISTRO == "kinetic" && (GAZEBO_VERSION == "" || GAZEBO_VERSION == "7")) {
+  //   GAZEBO_VERSION = "7";
+  // } else if (ROS_DISTRO == "kinetic" && GAZEBO_VERSION == "9") {
+  //   await setup_gazebo_source();
+  // } else if (ROS_DISTRO == "melodic" && (GAZEBO_VERSION == "" || GAZEBO_VERSION == "9")) {
+  //   GAZEBO_VERSION = "9";
+  //   await setup_gazebo_source();
+  // } else if (ROS_DISTRO == "dashing" && (GAZEBO_VERSION == "" || GAZEBO_VERSION == "9")) {
+  //   GAZEBO_VERSION = "9";
+  //   await setup_gazebo_source();
+  // } else if (ROS_DISTRO == "foxy" && (GAZEBO_VERSION == "" || GAZEBO_VERSION == "11")) {
+  //   GAZEBO_VERSION = "11";
+  //   await setup_gazebo_source();
+  // } else {
+  //   core.setFailed(`Invalid ROS and Gazebo combination`);
+  // }
   if (GENERATE_SOURCES == 'true') {
     await prepare_sources();
   }
