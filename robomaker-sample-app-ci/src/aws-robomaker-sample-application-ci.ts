60d024637f27da981fe84c2edeeee2284749ab8e
chengke <chengke@uefee1b00be3a55.ant.amazon.com>
2021-06-07 10:09:04 -0700

    ros not sourcing

--- robomaker-sample-app-ci/src/aws-robomaker-sample-application-ci.ts
+++ robomaker-sample-app-ci/src/aws-robomaker-sample-application-ci.ts
@@ -212,8 +212,9 @@ async function prepare_sources() {
 
 async function build() {
   try {
-    await exec.exec("rosdep", ["install", "--from-paths", ".", "--ignore-src", "-r", "-y", "--rosdistro", ROS_DISTRO], getWorkingDirExecOptions());
-    console.log(`Building the following packages: ${PACKAGES}`);
+    // await exec.exec("rosdep", ["install", "--from-paths", ".", "--ignore-src", "-r", "-y", "--rosdistro", ROS_DISTRO], getWorkingDirExecOptions());
+    // console.log(`Building the following packages: ${PACKAGES}`);
+    loadROSEnvVariables();
     await exec.exec("colcon", ["build", "--build-base", "build", "--install-base", "install"], getWorkingDirExecOptions());
   } catch (error) {
     core.setFailed(error.message);
